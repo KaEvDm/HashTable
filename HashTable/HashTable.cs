ea87e9478a692d964bf8a9da0803776f08bb7811
KaEvDm <evgeny.kartashow@gmail.com>
2018-09-26 18:24:11 +0300

    init

---
+++ HashTable/HashTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public abstract class HashTable
+    {
+        public abstract void Insert(Item item);
+        public abstract Item Search(int key);
+        public abstract void Delete(int key);
+    }
+}
