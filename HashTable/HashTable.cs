8099110442d50bf1064b07971a2e3263a5338190
KaEvDm <evgeny.kartashow@gmail.com>
2018-09-28 00:07:36 +0300

    (Code) ввёл ограничение для количества элементов в HashTable

--- HashTable/HashTable.cs
+++ HashTable/HashTable.cs
@@ -5,12 +5,13 @@ namespace HashTable
     public abstract class HashTable<TKey, TValue>
     {
         //SecretHashCode определяется как "KaEvDm".GetHashCode();
-        protected const int SecretHashCode = -1474368688;
+        private const int SecretHashCode = -1474368688;
+        private const int Size = 256;
 
         public abstract void Add(Item<TKey, TValue> item);
         public abstract TValue Search(TKey key);
         public abstract void Delete(TKey key);
 
-        protected int GetHash(TKey key) => key.GetHashCode() ^ SecretHashCode;
+        protected int GetHash(TKey key) => (key.GetHashCode() ^ SecretHashCode) % Size;
     }
 }
