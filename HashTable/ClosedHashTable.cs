bc27d54227cf90b56202c5259a5f39931dab17e6
KaEvDm <evgeny.kartashow@gmail.com>
2018-09-28 00:03:25 +0300

    (bug) перепутал ClosedHashTable и OpenHashTable, исправил

--- HashTable/ClosedHashTable.cs
+++ HashTable/ClosedHashTable.cs
@@ -6,37 +6,14 @@ namespace HashTable
 {
     public sealed class ClosedHashTable<TKey, TValue> : HashTable<TKey, TValue>
     {
-        private Dictionary<int, List<Item<TKey, TValue>>> items;
-
-        public ClosedHashTable()
-        {
-            items = new Dictionary<int, List<Item<TKey, TValue>>>();
-        }
-
         public override void Add(Item<TKey, TValue> item)
         {
-            var hash = GetHash(item.Key);
-
-            if (items.ContainsKey(hash))
-            {
-                if (items[hash].SingleOrDefault(i => i.Key.Equals(item.Key)) != null)
-                    throw new ArgumentException($"Хеш-таблица уже содержит элемент с ключом {item.Key}." +
-                                                " Ключ должен быть уникален.", nameof(item.Key));
-                else items[hash].Add(item);
-            }
-            else items.Add(hash, new List<Item<TKey, TValue>> { item });
+            throw new NotImplementedException();
         }
 
         public override TValue Search(TKey key)
         {
-            var hash = GetHash(key);
-
-            if (!items.ContainsKey(hash)) return default(TValue);
-
-            var item = items[hash].SingleOrDefault(i => i.Key.Equals(key));
-
-            if (item != null) return item.Value;
-            else return default(TValue);
+            throw new NotImplementedException();
         }
 
         public override void Delete(TKey key)
--- HashTable/OpenHashTable.cs
+++ HashTable/OpenHashTable.cs
@@ -6,14 +6,37 @@ namespace HashTable
 {
     public class OpenHashTable<TKey, TValue> : HashTable<TKey, TValue>
     {
+        private Dictionary<int, List<Item<TKey, TValue>>> items;
+
+        public OpenHashTable()
+        {
+            items = new Dictionary<int, List<Item<TKey, TValue>>>();
+        }
+
         public override void Add(Item<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            var hash = GetHash(item.Key);
+
+            if (items.ContainsKey(hash))
+            {
+                if (items[hash].SingleOrDefault(i => i.Key.Equals(item.Key)) != null)
+                    throw new ArgumentException($"Хеш-таблица уже содержит элемент с ключом {item.Key}." +
+                                                " Ключ должен быть уникален.", nameof(item.Key));
+                else items[hash].Add(item);
+            }
+            else items.Add(hash, new List<Item<TKey, TValue>> { item });
         }
 
         public override TValue Search(TKey key)
         {
-            throw new NotImplementedException();
+            var hash = GetHash(key);
+
+            if (!items.ContainsKey(hash)) return default(TValue);
+
+            var item = items[hash].SingleOrDefault(i => i.Key.Equals(key));
+
+            if (item != null) return item.Value;
+            else return default(TValue);
         }
 
         public override void Delete(TKey key)
